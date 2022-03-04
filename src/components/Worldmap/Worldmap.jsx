c9cbded381f2fe1c4314b28db3588aba6c521bf6
amathew <angel.mathew@jetblue.com>
2022-03-03 20:04:24 -0500

    cleanup

--- src/components/Worldmap/Worldmap.jsx
+++ src/components/Worldmap/Worldmap.jsx
@@ -15,27 +15,22 @@ const Worldmap=()=>{
     
 
     const drawMap=()=>{
-      
+      console.log(data)
         const svg = d3.select('#map')
                     .append('svg')
                     .attr('width', width).attr('height', height);
         // Projection
         const projection = 
                         d3.geoMercator()
-                        // d3.geoEquirectangular() 
-                        // d3.geoAzimuthalEqualArea()
-                            // .scale(140)
-                            .translate([width / 2, height / 1.4]);
+                          .translate([width / 2, height / 1.4]);
+
         // Geographic path
         const path = d3.geoPath(projection);
     
         // To set colors to different countries
-        // let color = d3.scaleOrdinal(d3.schemeOrRd[5]);
-       
-        // Define color scale
         const color = d3.scaleThreshold()
-        .domain([1,2,3,4,5,6])
-        .range(d3.schemeOrRd[7]);
+        .domain([1,2,3,4,5,6,7])
+        .range(['lightblue','#b284be','#9966cc','#7c9ec3',' #702963','#856088','#4B0082','#c54b8c']);
     
         // Create tooltip
         const tooltip=d3.select('#map')
@@ -55,7 +50,7 @@ const Worldmap=()=>{
         const mouseMoveHandler = (event,d)=> {
       
             tooltip
-              .html('<strong>'+d.properties.admin+'</strong><br>'+d.properties.economy.slice(2))
+              .html('<strong>'+d.properties.admin+'</strong><br>'+d.properties.economy.slice(2)+'<br> #'+d.properties.economy.slice(0,1))
               .style(
                 'transform',
                 `translate(${(event.x)-10}px,${(event.y)-60}px)`)
@@ -66,8 +61,6 @@ const Worldmap=()=>{
             tooltip.style("opacity", 0)
           }
 
-        svg.append('g')
-        .attr('transform', `translate(40,310)`).call(ColorLegend,{colors:color})
        
         // Draw the map
         const g=svg.append('g')
@@ -76,9 +69,9 @@ const Worldmap=()=>{
                 .append('path')
                 countryMap.attr("class", function(d){ return "Country"+d.id } )
                 .attr('d', path)
-                .attr("fill", (i) => color(i.properties.economy.slice(0,1)))
+                .attr("fill", (i) =>{return color(i.properties.economy.slice(0,1))})
                 .attr("stroke", "white")
-                .attr("stroke-width", 0.5)
+                .attr("stroke-width", 0.2)
                 .on("mouseover",mouseOverHandler)
                 .on("mouseout", mouseOutHandler)
                 .on("mousemove", mouseMoveHandler)
@@ -86,7 +79,6 @@ const Worldmap=()=>{
 
 
     useEffect(()=>{
-      console.log("useEffect")
       //  const url= 'https://unpkg.com/world-atlas@1.1.4/world/50m.tsv';
       // const url= 'https://gist.githubusercontent.com/AngelMathew/75a4599246c6ba8cb96cd1a40ec14069/raw/1693a5a61841deaba55946c31916066c55c48af5/mapDetails.tsv';
       const url='https://gist.githubusercontent.com/AngelMathew/cba118c19f4956c8b9cde568fae6e517/raw/map.json';
@@ -108,6 +100,7 @@ const Worldmap=()=>{
         Object.assign(d.properties, rowById[d.id]);
       });
       setData(countries.features);
+      
     
     });   
 },[]);
